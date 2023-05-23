a02ef325ec380f30fbdedc43045a3da34246db62
Ignacio-Ucedo <ig.ucedo@gmail.com>
2023-05-23 20:06:56 -0300

    guia10 sin nreinas

--- ejercicios/ej01.go
+++ ejercicios/ej01.go
@@ -1,12 +1,81 @@
 package ejercicios
 
+import "fmt"
+
+//solo funciona 1 2 y 3 en test
 type NReinas struct {
+	tablero [][]bool
 }
 
 func NewNReinas(tam int) *NReinas {
-	return &NReinas{}
+
+	tablero := make([][]bool, tam)
+
+	for i := range tablero {
+		tablero[i] = make([]bool, tam)
+	}
+
+	return &NReinas{tablero: tablero}
 }
 
 func (nReinas NReinas) Resolver() int {
-	return 0
+	var soluciones int
+	var pasos []int // estos números almacenados significan las columnas en las que coloco cada reina.
+	//yendo desde la primera fila
+
+	for i := 0; i < len(nReinas.tablero); i++ {
+		for j := 0; j < len(nReinas.tablero[0]); j++ {
+
+			if nReinas.tablero[i][j] { //si está ocupado (porque volví en mis pasos) la desocupo
+				//para pasar a la sig. columna
+				nReinas.tablero[i][j] = false
+				fmt.Printf("Saqué la de (%v , %v)\n", i, j)
+				pasos = pasos[:len(pasos)-1]
+				if j == len(nReinas.tablero[0])-1 { // si la que saqué ya está en la última columna
+					//ubico la anterior en otra columna (siempre y cuando no esté parado en la primera fila)
+					if i > 0 {
+						i -= 2
+					} else { // si estoy en la primera fila se terminaron las posibles soluciones
+						return soluciones
+					}
+
+				}
+			} else if esFactible(i, j, pasos) { // si es factible colocarla en la columna j
+				nReinas.tablero[i][j] = true // La coloco (aclaración: falso: libre , true: ocupado)
+				fmt.Printf("Puse una en (%v , %v)\n", i, j)
+				pasos = append(pasos, j)
+				if i == len(nReinas.tablero)-1 { // si coloqué la última
+					soluciones++
+					fmt.Print("\nSumé una solución\n\n")
+					if i > 0 {
+						i -= 2 //vuelvo en mis pasos siempre y cuando no esté en la primera fila (fila 0)
+					}
+				}
+
+				break
+			}
+
+			if j == len(nReinas.tablero[0])-1 { // si no la pude ubicar en ninguna columna
+				//ubico la anterior en otra columna
+				if i > 0 {
+					i -= 2
+				}
+
+			}
+		}
+	}
+
+	return soluciones
+}
+
+func esFactible(fila int, columna int, reinas []int) bool {
+
+	for i := 0; i < len(reinas); i++ {
+		if reinas[i] == columna || fila+columna == i+reinas[i] || fila-columna == i-reinas[i] {
+			return false
+		}
+	}
+
+	return fila != len(reinas)-1 //si estoy parado en la misma fila de una que ya coloqué
+	// return true
 }
--- ejercicios/ej03.go
+++ ejercicios/ej03.go
@@ -1,17 +1,60 @@
 package ejercicios
 
+import (
+	"math"
+)
+
 type Item struct {
 	valor int
 	peso  int
 }
 
 type Mochila struct {
+	tablero [][]int
+	items   []Item
 }
 
 func NewMochila(items []Item, capacidad int) *Mochila {
-	return &Mochila{}
+
+	tablero := make([][]int, len(items))
+
+	for i := range tablero {
+		tablero[i] = make([]int, capacidad)
+	}
+
+	return &Mochila{tablero: tablero, items: items}
 }
 
 func (mochila Mochila) Resolver() int {
-	return 0
+
+	for i := 0; i < len(mochila.tablero); i++ {
+		for j := 0; j < len(mochila.tablero[i]); j++ {
+			capacidadActual := j + 1
+			if i == 0 {
+				if mochila.items[0].peso <= capacidadActual {
+					mochila.tablero[i][j] = mochila.items[0].valor
+				}
+			} else {
+
+				noIncluyoItem := mochila.tablero[i-1][j]
+				var incluyoItem int
+
+				if mochila.items[i].peso <= capacidadActual {
+					capacidadRestante := capacidadActual - mochila.items[i].peso
+					if capacidadRestante == 0 {
+						incluyoItem = mochila.items[i].valor
+					} else {
+						incluyoItem = mochila.items[i].valor + mochila.tablero[i-1][capacidadRestante-1]
+					}
+
+				}
+				mochila.tablero[i][j] = int(math.Max(float64(incluyoItem), float64(noIncluyoItem)))
+			}
+		}
+	}
+
+	return mochila.tablero[len(mochila.items)-1][len(mochila.tablero[0])-1]
 }
+
+//https://www.youtube.com/watch?v=8LusJS5-AGo
+//la columna 0 es innecesaria
